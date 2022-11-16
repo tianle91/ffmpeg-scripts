5ad05fae00d7ce0abad48e339ece04bd9376cc86
Tianle Chen <tianlechen@gmail.com>
2022-11-16 14:43:08 -0500

    Revert "copy options to reduce cpu"
    This reverts commit 65b704af512eca1c842afa7037f26021614c5943.

--- run_batch.sh
+++ run_batch.sh
@@ -1,5 +1,5 @@
 for path in /input/*.mkv; do
     fname=$(basename "$path")
     # echo /output/$fname
-    ffmpeg -i "$path" -c:v copy -c:a copy -vf "transpose=1" "/output/$fname"
+    ffmpeg -i "$path" -vf "transpose=1" "/output/$fname"
 done
