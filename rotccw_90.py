7b0e5304ce059a7d1611536ea4271bcec9c1d3b9
Tianle Chen <tianlechen@gmail.com>
2023-07-12 17:34:38 -0400

    a bit more logic and fix command

--- rotccw_90.py
+++ rotccw_90.py
@@ -2,8 +2,12 @@ from glob import glob
 from pathlib import Path
 import os
 
+excluded_extensions = ['.py',]
 l = glob('*')
 for p in l:
-    output_path = Path(p).stem + ' (rotccw_90)' + Path(p).suffix
-    print(f'Rotating counter-clockwise 90: {p} -> {output_path}')
-    os.system(f'ffmpeg -i {p} -vf "transpose=2" {output_path}')
+    if Path(p).suffix in excluded_extensions:
+        continue
+    else:
+        output_path = Path(p).stem + '_(rotccw_90)' + Path(p).suffix
+        print(f'Rotating counter-clockwise 90: {p} -> {output_path}')
+        os.system(f'ffmpeg -i "{p}" -vf "transpose=2" "{output_path}"')
--- rotcw_90.py
+++ rotcw_90.py
@@ -2,8 +2,12 @@ from glob import glob
 from pathlib import Path
 import os
 
+excluded_extensions = ['.py',]
 l = glob('*')
 for p in l:
-    output_path = Path(p).stem + ' (rotcw_90)' + Path(p).suffix
-    print(f'Rotating clockwise 90: {p} -> {output_path}')
-    os.system(f'ffmpeg -i {p} -vf "transpose=1" {output_path}')
+    if Path(p).suffix in excluded_extensions:
+        continue
+    else:
+        output_path = Path(p).stem + '_(rotcw_90)' + Path(p).suffix
+        print(f'Rotating clockwise 90: {p} -> {output_path}')
+        os.system(f'ffmpeg -i "{p}" -vf "transpose=1" "{output_path}"')
