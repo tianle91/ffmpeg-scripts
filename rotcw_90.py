a796377df226de45d36b273b612003c877e31fe0
Tianle Chen <tianlechen@gmail.com>
2023-07-13 09:24:35 -0400

    specifically parse included extensions

--- rotcw_90.py
+++ rotcw_90.py
@@ -4,12 +4,19 @@ import os
 
 vf_argument = 'transpose=1'
 suffix = '(rotcw_90)'
-excluded_extensions = ['.py',]
-os.makedirs('output', exist_ok=True)
+included_extensions = ['mp4', 'mkv']
+output_folder = 'output'
 
-l = glob('*')
-for p in l:
+os.makedirs(output_folder, exist_ok=True)
+
+l = sorted([
+    *glob(f'*.{ext}')
+    for ext in included_extensions
+])
+
+for i, p in enumerate(l):
     p_suffix = Path(p).suffix
     if p_suffix not in excluded_extensions:
-        output_path = os.path.join('output', Path(p).stem + f' {suffix}' + p_suffix)
+        output_path = os.path.join(output_folder, Path(p).stem + f' {suffix}' + p_suffix)
+        print(f'[{i} / {len(l)}] Processing: {p} --> {output_path}')
         os.system(f'ffmpeg -hwaccel cuda -i "{p}" -vf "{vf_argument}" "{output_path}"')
