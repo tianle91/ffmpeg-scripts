0a5de5d06d9be98af2d019e35d0910cb8103f69d
Tianle Chen <tianlechen@gmail.com>
2023-07-17 07:43:47 -0400

    Update rotcw_90.py

--- rotcw_90.py
+++ rotcw_90.py
@@ -9,14 +9,13 @@ output_folder = 'output'
 
 os.makedirs(output_folder, exist_ok=True)
 
-l = sorted([
-    *glob(f'*.{ext}')
-    for ext in included_extensions
-])
+l = []
+for ext in included_extensions:
+    l += list(glob(f'*.{ext}'))
+l.sort()
 
 for i, p in enumerate(l):
     p_suffix = Path(p).suffix
-    if p_suffix not in excluded_extensions:
-        output_path = os.path.join(output_folder, Path(p).stem + f' {suffix}' + p_suffix)
-        print(f'[{i} / {len(l)}] Processing: {p} --> {output_path}')
-        os.system(f'ffmpeg -hwaccel cuda -i "{p}" -vf "{vf_argument}" "{output_path}"')
+    output_path = os.path.join(output_folder, Path(p).stem + f' {suffix}' + p_suffix)
+    print(f'[{i} / {len(l)}] Processing: {p} --> {output_path}')
+    os.system(f'ffmpeg -hwaccel cuda -i "{p}" -vf "{vf_argument}" "{output_path}"')
