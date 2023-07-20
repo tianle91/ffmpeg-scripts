5b3a74a625570ca60c98d6f6db74678f2f4a885c
Tianle Chen <tianlechen@gmail.com>
2023-07-19 22:30:44 -0400

    Update rotccw_90.py

--- rotccw_90.py
+++ rotccw_90.py
@@ -4,12 +4,18 @@ import os
 
 vf_argument = 'transpose=2'
 suffix = '(rotccw_90)'
-excluded_extensions = ['.py',]
-os.makedirs('output', exist_ok=True)
+included_extensions = ['mp4', 'mkv']
+output_folder = 'output'
 
-l = glob('*')
-for p in l:
+os.makedirs(output_folder, exist_ok=True)
+
+l = []
+for ext in included_extensions:
+    l += list(glob(f'*.{ext}'))
+l.sort()
+
+for i, p in enumerate(l):
     p_suffix = Path(p).suffix
-    if p_suffix not in excluded_extensions:
-        output_path = os.path.join('output', Path(p).stem + f' {suffix}' + p_suffix)
-        os.system(f'ffmpeg -hwaccel cuda -i "{p}" -vf "{vf_argument}" "{output_path}"')
+    output_path = os.path.join(output_folder, Path(p).stem + f' {suffix}' + p_suffix)
+    print(f'[{i} / {len(l)}] Processing: {p} --> {output_path}')
+    os.system(f'ffmpeg -hwaccel cuda -i "{p}" -vf "{vf_argument}" "{output_path}"')
