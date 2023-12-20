3e1237e05b23503ad5607ef75d555fc271426c74
Tianle Chen <tianlechen@gmail.com>
2023-12-20 18:06:45 -0500

    manual confirmation for concatenation

--- concat_mp4.py
+++ concat_mp4.py
@@ -4,12 +4,25 @@ from pathlib import Path
 import os
 
 l = sorted(list(glob('*.mp4')))
-prefixes = sorted(list({Path(s).stem[:-1] for s in l}))
-assert len(prefixes) == 1, 'Need the same prefixes:\n' + '\n'.join(prefixes)
-with open('l.txt', 'w') as f:
-    for s in l:
-       f.write(f"file '{s}'\n")
-
-output_fname = f"{prefixes.pop().strip().strip('-_')}.mp4"
-print(f'Writing to: {output_fname}')
-os.system(f'ffmpeg -f concat -safe 0 -i l.txt -c copy {output_fname}')
+prompt_str = (
+    'Will concatenate in the following order:\n'
+    + '\n'.join([f'{i:4.0f}: {s}'for i, s in enumerate(l)])
+    + '\nEnter Y to proceed: '
+)
+if input(prompt_str).lower() == 'y':
+    # find prefix
+    max_i = 0
+    for i in range(min([len(s) for s in l])):
+        if len({s[:i] for s in l}) == 1:
+            max_i = i
+    # concatenate if prefix is found
+    if max_i == 0:
+        raise ValueError('No common prefix found!')
+    else:
+        prefix = l[0][:max_i]
+        with open('l.txt', 'w') as f:
+            for s in l:
+            f.write(f"file '{s}'\n")
+        output_fname = f"{prefix.strip().strip('-_')}.mp4"
+        print(f'Writing to: {output_fname}')
+        os.system(f'ffmpeg -f concat -safe 0 -i l.txt -c copy {output_fname}')
