577c551d10f65da4052cea16e9da64af76c42b2e
Tianle Chen <tianlechen@gmail.com>
2023-12-22 09:19:00 -0500

    fix indent error

--- concat_mp4.py
+++ concat_mp4.py
@@ -9,20 +9,20 @@ prompt_str = (
     + '\n'.join([f'{i:4.0f}: {s}'for i, s in enumerate(l)])
     + '\nEnter Y to proceed: '
 )
-if input(prompt_str).lower() == 'y':
+if input(prompt_str).lower().strip() == 'y':
     # find prefix
     max_i = 0
     for i in range(min([len(s) for s in l])):
         if len({s[:i] for s in l}) == 1:
             max_i = i
-    # concatenate if prefix is found
     if max_i == 0:
-        raise ValueError('No common prefix found!')
+        prefix = 'no_prefix_found'
     else:
         prefix = l[0][:max_i]
-        with open('l.txt', 'w') as f:
-            for s in l:
+    # concatenate using prefix
+    with open('l.txt', 'w') as f:
+        for s in l:
             f.write(f"file '{s}'\n")
-        output_fname = f"{prefix.strip().strip('-_')}.mp4"
-        print(f'Writing to: {output_fname}')
-        os.system(f'ffmpeg -f concat -safe 0 -i l.txt -c copy {output_fname}')
+    output_fname = f"{prefix.strip().strip('-_')}.mp4"
+    print(f'Writing to: {output_fname}')
+    os.system(f'ffmpeg -f concat -safe 0 -i l.txt -c copy {output_fname}')
