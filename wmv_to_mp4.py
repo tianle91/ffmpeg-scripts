1b49dc9b3f7867e231343f39f6ad1fa8eb4db0e6
Tianle Chen <tianlechen@gmail.com>
2024-12-06 14:45:02 -0500

    cuda option

--- wmv_to_mp4.py
+++ wmv_to_mp4.py
@@ -6,5 +6,6 @@ l = glob(f'*.wmv')
 for p in l:
     output_path = Path(p).stem + '.mp4'
     print(f'Converting to mp4: {p} -> {output_path}')
+    # add "-hwaccel cuda" to use cuda
     # add "-r 29" option if concatenation is having timeline desync issues where 29 is the fixed fps
     os.system(f'ffmpeg -y -i {p} -c:v libx264 -c:a mp3 -max_muxing_queue_size 9999 {output_path}')
